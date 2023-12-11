3e85f7baab492c0e21241e78a25d486193383991
anurodh <anurodhk82@gmail.com>
2023-12-11 23:56:42 +0530

    qr sample

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express= require('express');
 const app=express();
+const qrcode = require('qrcode');
+
 
 let PORT = process.env.PORT || 2023;
 
-app.get('/',(req,res)=>{
+app.get('/',async(req,res)=>{
     console.log("Server started");
-    res.send("DONE");
+    const upiDetails = 'upi://pay?pa=example@upi&pn=John%20Doe&mc=1234&tid=123456&tr=123456789&tn=Payment%20for%20goods&cu=INR';
+
+    // Generate QR code
+    const qrCode = await qrcode.toDataURL(upiDetails);
+    // res.send("DONE");
+    res.send(`<img src="${qrCode}" alt="UPI QR Code">`);
+
 })
 
 app.listen(PORT,()=>{
--- src/script.js
+++ src/script.js
@@ -1,9 +1,15 @@
 
-const vidElem= document.getElementById('vidId');
+// const vidElem= document.getElementById('vidId');
 
-const qrScanner = new QrScanner(
-    videoElem,
-    result => console.log('decoded qr code:', result),
-    { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
-);
-qrScanner.start();
+// const qrScanner = new QrScanner(
+//     videoElem,
+//     result => console.log('decoded qr code:', result),
+//     { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
+// );
+// qrScanner.start();
+
+const qrcode = require('qrcode');
+
+qrcode.toDataURL('img/png',{type:'terminal'},(err,url)=>{
+    console.log(url);
+})

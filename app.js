4532554012b100b31640ca4755f7af62a4d313ce
anurodh <anurodhk82@gmail.com>
2023-12-12 00:30:31 +0530

    qr added and working

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ let PORT = process.env.PORT || 2023;
 
 app.get('/',async(req,res)=>{
     console.log("Server started");
-    const upiDetails = 'upi://pay?pa=example@upi&pn=John%20Doe&mc=1234&tid=123456&tr=123456789&tn=Payment%20for%20goods&cu=INR';
+    const upiDetails = 'upi://pay?pa=8193045263@paytm&pn=Anurodh%20Kumar&mc=1234&tid=123456&tr=123456789&tn=Payment%20for%20goods&cu=INR';
 
-    // Generate QR code
-    const qrCode = await qrcode.toDataURL(upiDetails);
-    // res.send("DONE");
-    res.send(`<img src="${qrCode}" alt="UPI QR Code">`);
+    // // Generate QR code
+    // console.log(upiDetails);
+    // const qrCode = await qrcode.toDataURL(upiDetails);
+    // // res.send("DONE");
+    // res.send(`<img src="${qrCode}" alt="UPI QR Code">`);
+    const size=5;
+    qrcode.toString(upiDetails,{type:'terminal',scale:size},(err,url)=>{
+        console.log(url);
+        // res.send(`<img src="${url}" alt="UPI QR Code">`);
+    });
+    const qrCode= qrcode.toDataURL(upiDetails,(err,url)=>{
+        // console.log(url);
+        res.send(`<img src="${url}" alt="UPI QR Code">`);
+
+    });
+
 
 })
 

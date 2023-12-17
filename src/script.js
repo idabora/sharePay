3ddc3fcd88bccfa935e8deb4e63631272c45717c
anurodh <anurodhk82@gmail.com>
2023-12-17 17:24:48 +0530

    qr-scanner module, still have things to fix

--- src/script.js
+++ src/script.js
@@ -1,15 +1,27 @@
+// const { JSDOM } = require('jsdom');
+// import QrScanner from 'path/to/qr-scanner.min.js'; // if using plain es6 import
+// import QrScanner from 'qr-scanner'; // if installed via package and bundling with a module bundler like webpack or rollup
+// const jsdom = new JSDOM('<!DOCTYPE html><html><body><div id="vidId"></div></body></html>');
+// const window = jsdom.window;
+// const document = window.document;
 
-// const vidElem= document.getElementById('vidId');
+window.onload = () => {
+    const videoElem = document.getElementById('vidId');
 
-// const qrScanner = new QrScanner(
-//     videoElem,
-//     result => console.log('decoded qr code:', result),
-//     { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
-// );
-// qrScanner.start();
+    if (videoElem) {
+        const qrScanner = new QrScanner(videoElem, result => {
+            // This callback function is called when a QR code is successfully scanned
+            console.log('QR code scanned:', result);
+            returnDetailedScanResult: true
+            // You can handle the scanned QR code result as needed
+        });
 
-const qrcode = require('qrcode');
-
-qrcode.toDataURL('img/png',{type:'terminal'},(err,url)=>{
-    console.log(url);
-})
+        // Start the camera
+        qrScanner.start();
+    }
+};
+// window.addEventListener("load", (event) => {
+//     console.log("Loading Done........");
+//     const videoElem = document.getElementById('vidId');
+//     console.log(videoElem.textContent); 
+//  });

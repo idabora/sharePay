8fa93a6966e1111ec08e409fc165cb5738c83bbc
anurodh <anurodhk82@gmail.com>
2023-12-25 23:41:12 +0530

    ignore this one

--- app.js
+++ app.js
@@ -1,34 +1,59 @@
-const express= require('express');
-const app=express();
-const qrcode = require('qrcode');
-
+const express = require('express');
+const app = express();
+// const scriptFile=require('./src/script')
+const path = require('path');
+const qrScanner = require('qr-scanner');
+const staticpath = path.join(__dirname, './src')
+app.use(express.static(staticpath));
+app.use(express.json())
 
 let PORT = process.env.PORT || 2023;
 
-app.get('/',async(req,res)=>{
-    console.log("Server started");
-    const upiDetails = 'upi://pay?pa=8193045263@paytm&pn=Anurodh%20Kumar&mc=1234&tid=123456&tr=123456789&tn=Payment%20for%20goods&cu=INR';
 
-    // // Generate QR code
-    // console.log(upiDetails);
-    // const qrCode = await qrcode.toDataURL(upiDetails);
-    // // res.send("DONE");
-    // res.send(`<img src="${qrCode}" alt="UPI QR Code">`);
-    const size=5;
-    qrcode.toString(upiDetails,{type:'terminal',scale:size},(err,url)=>{
-        console.log(url);
-        // res.send(`<img src="${url}" alt="UPI QR Code">`);
-    });
-    const qrCode= qrcode.toDataURL(upiDetails,(err,url)=>{
-        // console.log(url);
-        res.send(`<img src="${url}" alt="UPI QR Code">`);
+app.get('/', (req, res) => {
+    console.log(staticpath);
+    res.sendFile(path.join(__dirname, './src/index.html'));
+});
 
-    });
+app.post('/dothat', (req, res) => {
+    try{
 
-
+        console.log("do that");
+        console.log(req.body)
+    //     const videoInfo = req.body;
+        
+    //     let videoTag=`<html><head></head><body><video id=${videoInfo.id} ></video></body></html>`;
+    //     const QrScanner = new qrScanner(videoTag, result => {
+    //     // This callback function is called when a QR code is successfully scanned
+    //     console.log('QR code scanned:', result);
+    //     if (result?.data) {
+    //         let a = JSON.stringify(result.data);
+    //         let index = a.indexOf('&');
+    //         let upiId = a.slice(14, index);
+    //         let userName = a.slice(index + 4, a.lastIndexOf('&'));
+    //         console.log(upiId);
+    //         console.log(userName)
+    //         QrScanner.stop();
+    //     }
+    //     // You can handle the scanned QR code result as needed
+    // }, {
+    //     // returnDetailedScanResult: true,
+    //     onDecodeError: (error) => {
+    //         console.error('QR code scan error:', error);
+    //         // Handle the error as needed
+    //     },
+    //     prefferdCamera: 'User',
+    //     highlightScanRegion: true,
+    //     maxScansPerSecond: 3
+    //     // calculateScanRegion: 20
+    // });
+    res.send('DO THATs')
+}catch(err){
+    console.log(err)
+}
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server Listening on port http://localhost:${PORT}`)
 })
 
--- src/script.js
+++ src/script.js
@@ -1,34 +1,84 @@
+// // client.js
+// function doThat() {
+//     // Access the video element
+//     let videoElem = document.getElementById('vidId');
+
+//     // Your logic to interact with the video element goes here
+//     console.log('Accessed video element:', videoElem);
+//     const videoInfo = {
+//         id: videoElem.id
+//         // Add any other relevant information you need
+//     };
+//     // Make an AJAX request to the server
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('POST', '/dothat',true);
+//     xhr.setRequestHeader('Content-Type', 'application/json');
+
+//     xhr.onreadystatechange = function () {
+//         console.log(xhr.readyState);
+//     };
+//     console.log("here");
+//     // videoElem=`<video id=${videoInfo.id}>vid</video>`;
+//     // videoElem="hes"
+//     // console.log("here");
+//     xhr.send(JSON.stringify(videoInfo));
+// }
+
+
 
 window.onload = () => {
     const videoElem = document.getElementById('vidId');
-
+    let userName;
+    let upiId;
     if (videoElem) {
         const qrScanner = new QrScanner(videoElem, result => {
             // This callback function is called when a QR code is successfully scanned
             console.log('QR code scanned:', result);
-            if(result?.data){
-                let a=JSON.stringify(result.data);
-                let index=a.indexOf('&');
-                let upiId=a.slice(14,index);
-                let userName=a.slice(index+4,a.lastIndexOf('&'));
-                console.log(upiId);
-                console.log(userName)
+            if (result?.data) {
+                // let data=JSON.stringify(result.data)
+                let data=result.data
+                let payeeAdd = data.indexOf('pa=');
+                let payeeName = data.indexOf('pn=');
+                let andInd = data.indexOf('&');
+                userInd = (data.length > data.lastIndexOf('&')+1) ? data.lastIndexOf('&'):data.length-1;
+                console.log("--",userInd);
+                upiId = data.slice(payeeAdd + 3, andInd);
+
+                userName = data.slice(payeeName + 3, userInd);
+                console.log("upi-",upiId);
+                console.log(payeeName,userInd);
+                console.log("usernmae-",userName);
                 qrScanner.stop();
             }
             // You can handle the scanned QR code result as needed
         }, {
             // returnDetailedScanResult: true,
-            onDecodeError:  (error) => {
+            onDecodeError: (error) => {
                 console.error('QR code scan error:', error);
                 // Handle the error as needed
             },
             prefferdCamera: 'User',
             highlightScanRegion: true,
-            maxScansPerSecond:3
+            maxScansPerSecond: 3
             // calculateScanRegion: 20
         });
 
-        console.log(qrScanner.start());
+        qrScanner.start();
 
     }
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", '/dothat', true);
+
+    xhr.onreadystatechange = () => {
+        console.log(xhr.status);
+        console.log(xhr.readyState);
+    }
+    let userObj = {
+        userName,
+        upiId
+    }
+    xhr.send(JSON.stringify(userObj))
+
 };

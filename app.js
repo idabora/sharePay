ca0d8e0e220f661eb25b97a807730ac028b25408
anurodh <anurodhk82@gmail.com>
2023-12-30 16:54:53 +0530

    upi fetched and redirected to dothat(temp) route

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const path = require('path');
 const qrScanner = require('qr-scanner');
 const staticpath = path.join(__dirname, './src')
 app.use(express.static(staticpath));
-app.use(express.json())
+app.use(express.json());
+app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
+// app.use(express.static(staticpath, { 'Content-Type': 'application/javascript' }));
+
+
 
 let PORT = process.env.PORT || 2023;
 
 
 app.get('/', (req, res) => {
     console.log(staticpath);
-    res.sendFile(path.join(__dirname, './src/index.html'));
+    res.sendFile(path.join(__dirname, '/src/index.html'));
 });
 
 app.post('/dothat', (req, res) => {
@@ -22,33 +26,9 @@ app.post('/dothat', (req, res) => {
         console.log(req.body)
     //     const videoInfo = req.body;
         
-    //     let videoTag=`<html><head></head><body><video id=${videoInfo.id} ></video></body></html>`;
-    //     const QrScanner = new qrScanner(videoTag, result => {
-    //     // This callback function is called when a QR code is successfully scanned
-    //     console.log('QR code scanned:', result);
-    //     if (result?.data) {
-    //         let a = JSON.stringify(result.data);
-    //         let index = a.indexOf('&');
-    //         let upiId = a.slice(14, index);
-    //         let userName = a.slice(index + 4, a.lastIndexOf('&'));
-    //         console.log(upiId);
-    //         console.log(userName)
-    //         QrScanner.stop();
-    //     }
-    //     // You can handle the scanned QR code result as needed
-    // }, {
-    //     // returnDetailedScanResult: true,
-    //     onDecodeError: (error) => {
-    //         console.error('QR code scan error:', error);
-    //         // Handle the error as needed
-    //     },
-    //     prefferdCamera: 'User',
-    //     highlightScanRegion: true,
-    //     maxScansPerSecond: 3
-    //     // calculateScanRegion: 20
-    // });
     res.send('DO THATs')
 }catch(err){
+    console.log("*****ERROR*****");
     console.log(err)
 }
 })
--- src/script.js
+++ src/script.js
@@ -1,56 +1,80 @@
-// // client.js
-// function doThat() {
-//     // Access the video element
-//     let videoElem = document.getElementById('vidId');
 
-//     // Your logic to interact with the video element goes here
-//     console.log('Accessed video element:', videoElem);
-//     const videoInfo = {
-//         id: videoElem.id
-//         // Add any other relevant information you need
-//     };
-//     // Make an AJAX request to the server
+// let upiId;
+// window.onload = () => {
+
+//     const videoElem = document.getElementById('vidId');
+//     let userName;
+//     // let upiId;
+//     if (videoElem) {
+//         const qrScanner = new QrScanner(videoElem, result => {
+//             // This callback function is called when a QR code is successfully scanned
+//             console.log('QR code scanned:', result);
+//             if (result?.data) {
+//                 // let data=JSON.stringify(result.data)
+//                 let data = result.data
+//                 let payeeAdd = data.indexOf('pa=');
+//                 // let payeeName = data.indexOf('pn=');
+//                 let andInd = data.indexOf('&');
+//                 // userInd = (data.length > data.lastIndexOf('&')+1) ? data.lastIndexOf('&'):data.length-1;
+//                 // console.log("--",userInd);
+//                 upiId = data.slice(payeeAdd + 3, andInd);
+
+//                 // userName = data.slice(payeeName + 3, userInd);
+//                 console.log("upi-", upiId);
+//                 // console.log(payeeName,userInd);
+//                 // console.log("usernmae-",userName);
+//                 qrScanner.stop();
+//             }
+//             // You can handle the scanned QR code result as needed
+//         }, {
+//             // returnDetailedScanResult: true,
+//             onDecodeError: (error) => {
+//                 console.error('QR code scan error:', error);
+//                 // Handle the error as needed
+//             },
+//             prefferdCamera: 'User',
+//             highlightScanRegion: true,
+//             maxScansPerSecond: 3
+//             // calculateScanRegion: 20
+//         });
+
+//         qrScanner.start();
+
+//     }
+// };
+
+// function dothat() {
+
 //     const xhr = new XMLHttpRequest();
-//     xhr.open('POST', '/dothat',true);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
 
-//     xhr.onreadystatechange = function () {
+//     xhr.open("POST", '/dothat', true);
+
+//     xhr.onreadystatechange = () => {
+//         console.log(xhr.status);
 //         console.log(xhr.readyState);
-//     };
-//     console.log("here");
-//     // videoElem=`<video id=${videoInfo.id}>vid</video>`;
-//     // videoElem="hes"
-//     // console.log("here");
-//     xhr.send(JSON.stringify(videoInfo));
+//     }
+
+//     xhr.send(upiId)
+
 // }
+// window.dothat=dothat;
 
 
+
+
+
+let upiId; // Declare upiId in a global scope
 
 window.onload = () => {
     const videoElem = document.getElementById('vidId');
-    let userName;
-    let upiId;
+
     if (videoElem) {
         const qrScanner = new QrScanner(videoElem, result => {
-            // This callback function is called when a QR code is successfully scanned
-            console.log('QR code scanned:', result);
             if (result?.data) {
-                // let data=JSON.stringify(result.data)
-                let data=result.data
-                let payeeAdd = data.indexOf('pa=');
-                let payeeName = data.indexOf('pn=');
-                let andInd = data.indexOf('&');
-                userInd = (data.length > data.lastIndexOf('&')+1) ? data.lastIndexOf('&'):data.length-1;
-                console.log("--",userInd);
-                upiId = data.slice(payeeAdd + 3, andInd);
-
-                userName = data.slice(payeeName + 3, userInd);
-                console.log("upi-",upiId);
-                console.log(payeeName,userInd);
-                console.log("usernmae-",userName);
+                upiId = extractUpiId(result.data);
+                console.log(upiId)
                 qrScanner.stop();
             }
-            // You can handle the scanned QR code result as needed
         }, {
             // returnDetailedScanResult: true,
             onDecodeError: (error) => {
@@ -59,26 +83,38 @@ window.onload = () => {
             },
             prefferdCamera: 'User',
             highlightScanRegion: true,
-            maxScansPerSecond: 3
+            maxScansPerSecond: 3,
             // calculateScanRegion: 20
         });
 
         qrScanner.start();
-
     }
+};
 
+function dothat() {
+    console.log("======>>>>>",upiId)
     const xhr = new XMLHttpRequest();
-
+    
     xhr.open("POST", '/dothat', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
 
     xhr.onreadystatechange = () => {
         console.log(xhr.status);
         console.log(xhr.readyState);
     }
-    let userObj = {
-        userName,
-        upiId
-    }
-    xhr.send(JSON.stringify(userObj))
+    console.log(JSON.stringify(upiId),upiId)
+    let obj={};
+    obj['upiId']=upiId;
+    console.log(obj);
+    xhr.send(JSON.stringify(obj));
+}
 
-};
+// Extract UPI ID logic (you can modify this based on your actual data extraction logic)
+function extractUpiId(data) {
+    const payeeAdd = data.indexOf('pa=');
+    const andInd = data.indexOf('&');
+    return data.slice(payeeAdd + 3, andInd);
+}
+
+// Ensure dothat() is accessible globally
+window.dothat = dothat;
